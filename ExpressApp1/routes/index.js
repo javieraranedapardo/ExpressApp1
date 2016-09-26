80bce732fe9f5a37ad3fc3b0eb5bf149f9abb479
javieraranedapardo <javier.araneda@gmail.com>
2016-09-26 19:05:38 -0300

    second commit

--- ExpressApp1/routes/index.js
+++ ExpressApp1/routes/index.js
@@ -8,7 +8,25 @@ exports.index = function (req, res) {
 };
 
 exports.about = function (req, res) {
-    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page' });
+
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+        host: 'instance1.cnd201jodli4.us-west-2.rds.amazonaws.com',
+        user: 'root',
+        password: 'P1ssw4rd',
+        database: 'prueba'
+    });
+
+    connection.connect();
+    var filas = [{ Nombre: 'hola' }];
+
+    connection.query('SELECT * from Usuario', function (err, rows, fields) {
+        res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page', filas: rows });    
+    });
+
+    connection.end();
+
+    
 };
 
 exports.contact = function (req, res) {
